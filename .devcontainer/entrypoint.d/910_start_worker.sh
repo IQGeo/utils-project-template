d2e8fdf58839360117babdb4ab24127e5b809d15
Mike Strong <mike.strong@iqgeo.com>
2024-10-23 10:11:40 -0600

    Address PR review comments

--- .devcontainer/entrypoint.d/910_start_worker.sh
+++ .devcontainer/entrypoint.d/910_start_worker.sh
@@ -1,12 +1,12 @@
 #! /bin/bash -e
 
-if [[ -n "${TASK_WORKERS}" && "${TASK_WORKERS}" -gt 0 ]]; then
-    # Expected: start workers if TASK_WORKERS is set
+if [[ -n "${MYW_TASK_WORKERS}" && "${MYW_TASK_WORKERS}" -gt 0 ]]; then
+    # Expected: start workers if MYW_TASK_WORKERS is set
     # Requires: Redis service to be running and RQ_REDIS_URL be set
 
-    command="myw_task start --workers ${TASK_WORKERS}"
-    if [[ -n "${TASK_QUEUES}" ]]; then
-        command+=" --queues ${TASK_QUEUES}"
+    command="myw_task start --workers ${MYW_TASK_WORKERS}"
+    if [[ -n "${MYW_TASK_QUEUES}" ]]; then
+        command+=" --queues ${MYW_TASK_QUEUES}"
     fi
     
     $command &
