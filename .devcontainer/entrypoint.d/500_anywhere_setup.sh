61553a7e619d1feefcd7c0b14f12c675da1f2f24
Mike Strong <mike.strong@iqgeo.com>
2025-04-21 10:28:17 -0600

    Updated project template to provide anywhere access to public resources

---
+++ .devcontainer/entrypoint.d/500_anywhere_setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Ensure the target directories exist
+mkdir -p /opt/iqgeo/anywhere/modules/comms
+mkdir -p /opt/iqgeo/anywhere/modules/comsof
+mkdir -p /opt/iqgeo/anywhere/modules/comms_dev_db
+mkdir -p /opt/iqgeo/anywhere/modules/custom
+mkdir -p /opt/iqgeo/anywhere/locales
+mkdir -p /opt/iqgeo/anywhere/bundles
+
+# Copy files from different locations to /opt/iqgeo/anywhere
+cp -r /opt/iqgeo/platform/WebApps/myworldapp/core/native/nativeApp.html /opt/iqgeo/anywhere/nativeApp.html
+cp -r /opt/iqgeo/platform/WebApps/myworldapp/public/bundles/* /opt/iqgeo/anywhere/bundles/
+cp -r /opt/iqgeo/platform/WebApps/myworldapp/modules/comms/public/* /opt/iqgeo/anywhere/modules/comms/
+cp -r /opt/iqgeo/platform/WebApps/myworldapp/modules/comsof/public/* /opt/iqgeo/anywhere/modules/comsof/
+cp -r /opt/iqgeo/platform/WebApps/myworldapp/modules/comms_dev_db/public/* /opt/iqgeo/anywhere/modules/comms_dev_db/
+cp -r /opt/iqgeo/platform/WebApps/myworldapp/public/locales/* /opt/iqgeo/anywhere/locales/
+cp -r /opt/iqgeo/platform/WebApps/myworldapp/modules/custom/public/* /opt/iqgeo/anywhere/modules/custom/
+
+# Watch for changes in the bundles directory and copy the files when changes are detected
+# while inotifywait -r -e modify,create,delete,move /opt/iqgeo/platform/WebApps/myworldapp/public/bundles; do
+#     cp -r /opt/iqgeo/platform/WebApps/myworldapp/public/bundles/* /opt/iqgeo/anywhere/bundles/
+#     chown -R www-data:www-data /opt/iqgeo/anywhere/bundles
+# done
