8fb2199310a7bb03bfb7c8d75b6b1aca18cb2865
Andrew Wiegers <andrew@Andrews-MacBook-Pro.local>
2025-04-29 09:28:58 -0600

    eng/anywhere-development: add task to watch for changes then copy bundles outside of container to update locally running anywhere electron app.

--- .devcontainer/entrypoint.d/500_anywhere_setup.sh
+++ .devcontainer/entrypoint.d/500_anywhere_setup.sh
@@ -13,12 +13,6 @@ cp -r /opt/iqgeo/platform/WebApps/myworldapp/core/native/nativeApp.html /opt/iqg
 cp -r /opt/iqgeo/platform/WebApps/myworldapp/public/bundles/* /opt/iqgeo/anywhere/bundles/
 cp -r /opt/iqgeo/platform/WebApps/myworldapp/modules/comms/public/* /opt/iqgeo/anywhere/modules/comms/
 cp -r /opt/iqgeo/platform/WebApps/myworldapp/modules/comsof/public/* /opt/iqgeo/anywhere/modules/comsof/
-cp -r /opt/iqgeo/platform/WebApps/myworldapp/modules/comms_dev_db/public/* /opt/iqgeo/anywhere/modules/comms_dev_db/
+# cp -r /opt/iqgeo/platform/WebApps/myworldapp/modules/comms_dev_db/public/* /opt/iqgeo/anywhere/modules/comms_dev_db/
 cp -r /opt/iqgeo/platform/WebApps/myworldapp/public/locales/* /opt/iqgeo/anywhere/locales/
 cp -r /opt/iqgeo/platform/WebApps/myworldapp/modules/custom/public/* /opt/iqgeo/anywhere/modules/custom/
-
-# Watch for changes in the bundles directory and copy the files when changes are detected
-# while inotifywait -r -e modify,create,delete,move /opt/iqgeo/platform/WebApps/myworldapp/public/bundles; do
-#     cp -r /opt/iqgeo/platform/WebApps/myworldapp/public/bundles/* /opt/iqgeo/anywhere/bundles/
-#     chown -R www-data:www-data /opt/iqgeo/anywhere/bundles
-# done
