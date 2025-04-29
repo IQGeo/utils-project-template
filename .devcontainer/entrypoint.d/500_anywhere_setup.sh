f999066d345d286374f081eb08912fa197177266
Andrew Wiegers <andrew@Andrews-MacBook-Pro.local>
2025-04-29 13:10:40 -0600

    enh/anywhere-development: remove extra lines and add /native to .gitignore

--- .devcontainer/entrypoint.d/500_anywhere_setup.sh
+++ .devcontainer/entrypoint.d/500_anywhere_setup.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Ensure the target directories exist
-mkdir -p /opt/iqgeo/anywhere/modules/comms
-mkdir -p /opt/iqgeo/anywhere/modules/comsof
-mkdir -p /opt/iqgeo/anywhere/modules/comms_dev_db
-mkdir -p /opt/iqgeo/anywhere/modules/custom
 mkdir -p /opt/iqgeo/anywhere/locales
 mkdir -p /opt/iqgeo/anywhere/bundles
+## Start Section - 
+mkdir -p /opt/iqgeo/anywhere/modules/comms
+mkdir -p /opt/iqgeo/anywhere/modules/comsof
+## End Section
 
 # Copy files from different locations to /opt/iqgeo/anywhere
 cp -r /opt/iqgeo/platform/WebApps/myworldapp/core/native/nativeApp.html /opt/iqgeo/anywhere/nativeApp.html
 cp -r /opt/iqgeo/platform/WebApps/myworldapp/public/bundles/* /opt/iqgeo/anywhere/bundles/
-cp -r /opt/iqgeo/platform/WebApps/myworldapp/modules/comms/public/* /opt/iqgeo/anywhere/modules/comms/
-cp -r /opt/iqgeo/platform/WebApps/myworldapp/modules/comsof/public/* /opt/iqgeo/anywhere/modules/comsof/
-# cp -r /opt/iqgeo/platform/WebApps/myworldapp/modules/comms_dev_db/public/* /opt/iqgeo/anywhere/modules/comms_dev_db/
 cp -r /opt/iqgeo/platform/WebApps/myworldapp/public/locales/* /opt/iqgeo/anywhere/locales/
+## Start Section
+cp -r /opt/iqgeo/platform/WebApps/myworldapp/modules/comms/public/* /opt/iqgeo/anywhere/modules/comms/
 cp -r /opt/iqgeo/platform/WebApps/myworldapp/modules/custom/public/* /opt/iqgeo/anywhere/modules/custom/
+## END Section
