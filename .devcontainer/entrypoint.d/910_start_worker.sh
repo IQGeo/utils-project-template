07fb770c6021f17de33e494eb0af0a7a3713615a
Sam Frances <sam.frances@iqgeo.com>
2025-04-30 14:28:21 +0100

    PLAT-10597: start task worker with debug flag

--- .devcontainer/entrypoint.d/910_start_worker.sh
+++ .devcontainer/entrypoint.d/910_start_worker.sh
@@ -4,7 +4,7 @@ if [[ -n "${MYW_TASK_WORKERS}" && "${MYW_TASK_WORKERS}" -gt 0 ]]; then
     # Expected: start workers if MYW_TASK_WORKERS is set
     # Requires: Redis service to be running and RQ_REDIS_URL be set
 
-    command="myw_task start --workers ${MYW_TASK_WORKERS}"
+    command="myw_task start --workers ${MYW_TASK_WORKERS} --debug"
     if [[ -n "${MYW_TASK_QUEUES}" ]]; then
         command+=" --queues ${MYW_TASK_QUEUES}"
     fi
