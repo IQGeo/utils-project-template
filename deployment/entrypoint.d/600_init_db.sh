e9270f5ecf83d925f9b3843b9c6778ea292eccb1
Andrew Wiegers <andrew@Andrews-MacBook-Pro.local>
2025-05-02 10:36:38 -0600

    task/PLAT-11630: vscode: Cleaning up extra modifications.

--- deployment/entrypoint.d/600_init_db.sh
+++ deployment/entrypoint.d/600_init_db.sh
@@ -4,5 +4,4 @@
 
 # START SECTION db init - if you edit these lines manually note that your change will get lost if you run the IQGeo Project Update tool
 if ! myw_db $MYW_DB_NAME list versions --layout keys | grep myw_comms_schema | grep version=; then myw_db $MYW_DB_NAME install comms; fi
-if ! myw_db $MYW_DB_NAME list versions --layout keys | grep iqg_comsof_schema | grep version=; then myw_db $MYW_DB_NAME install comsof; fi
 # END SECTION
