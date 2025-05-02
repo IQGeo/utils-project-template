bb9be5d590d1f9000662e44f90037e93cb02d3ff
Andrew Wiegers <andrew@Andrews-MacBook-Pro.local>
2025-05-02 10:26:32 -0600

    task/PLAT-11630: vscode: Added tasks and launch commands to automatically watch the code base for development changes. Allowing for quicker development with the Electron app.

--- .devcontainer/entrypoint.d/500_anywhere_setup.sh
+++ .devcontainer/entrypoint.d/500_anywhere_setup.sh
@@ -3,16 +3,12 @@
 # Ensure the target directories exist
 mkdir -p /opt/iqgeo/anywhere/locales
 mkdir -p /opt/iqgeo/anywhere/bundles
-## Start Section - 
-mkdir -p /opt/iqgeo/anywhere/modules/comms
-mkdir -p /opt/iqgeo/anywhere/modules/comsof
-## End Section
+# Start Section - make direcetory for bundles
+# End Section
 
 # Copy files from different locations to /opt/iqgeo/anywhere
 cp -r /opt/iqgeo/platform/WebApps/myworldapp/core/native/nativeApp.html /opt/iqgeo/anywhere/nativeApp.html
 cp -r /opt/iqgeo/platform/WebApps/myworldapp/public/bundles/* /opt/iqgeo/anywhere/bundles/
 cp -r /opt/iqgeo/platform/WebApps/myworldapp/public/locales/* /opt/iqgeo/anywhere/locales/
-## Start Section
-cp -r /opt/iqgeo/platform/WebApps/myworldapp/modules/comms/public/* /opt/iqgeo/anywhere/modules/comms/
-cp -r /opt/iqgeo/platform/WebApps/myworldapp/modules/custom/public/* /opt/iqgeo/anywhere/modules/custom/
-## END Section
+# Start Section - copy bundles to docker volumnes
+# END Section
