e429f38ef331bbcb0b7b5eac663891bc2f3c7cb5
Andrew Wiegers <andrew@Mac.lan>
2025-05-19 16:44:28 -0600

    task/PLAT-11630: docker: fixing spelling error.

--- .devcontainer/entrypoint.d/500_anywhere_setup.sh
+++ .devcontainer/entrypoint.d/500_anywhere_setup.sh
@@ -11,6 +11,6 @@ mkdir -p /opt/iqgeo/anywhere/modules/comms
 cp -r /opt/iqgeo/platform/WebApps/myworldapp/core/native/nativeApp.html /opt/iqgeo/anywhere/nativeApp.html
 cp -r /opt/iqgeo/platform/WebApps/myworldapp/public/bundles/* /opt/iqgeo/anywhere/bundles/
 cp -r /opt/iqgeo/platform/WebApps/myworldapp/public/locales/* /opt/iqgeo/anywhere/locales/
-# START SECTION - copy bundles to docker volumnes
+# START SECTION - copy bundles to docker volumes
 cp -r /opt/iqgeo/platform/WebApps/myworldapp/modules/comms/public/* /opt/iqgeo/anywhere/modules/comms/
 # END SECTION
