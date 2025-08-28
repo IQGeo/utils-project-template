475040d1795f298451d74180dcc49fbf72edcb62
Andrew Wiegers <andrew@Mac.lan>
2025-08-27 21:01:53 -0600

    PLAT-12007: stashing changes

---
+++ deployment/entrypoint.d/610_upgrade_db.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# START SECTION db upgrade - if you edit these lines manually note that your change will get lost if you run the IQGeo Project Update tool
+myw_db $MYW_DB_NAME upgrade comms
+# END SECTION
