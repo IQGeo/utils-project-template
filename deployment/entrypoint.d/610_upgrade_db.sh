522ad6b6f7dc75f642a718cf7782aa84c0638684
Andrew Wiegers <andrew@Andrews-MacBook-Pro.local>
2025-09-02 16:39:44 -0600

    PLAT-12007: script: running script to include default modules.

--- deployment/entrypoint.d/610_upgrade_db.sh
+++ deployment/entrypoint.d/610_upgrade_db.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # START SECTION db upgrade - if you edit these lines manually note that your change will get lost if you run the IQGeo Project Update tool
+myw_db $MYW_DB_NAME upgrade custom
 myw_db $MYW_DB_NAME upgrade comms
 # END SECTION
