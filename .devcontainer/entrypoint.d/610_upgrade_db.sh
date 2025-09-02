0e5ba6f6120d88dc7883bb64aa3b6db80f5ffd34
Andrew Wiegers <andrew@Andrews-MacBook-Pro.local>
2025-09-02 16:24:47 -0600

    PLAT-12007: script: add upgrade script to entrypoint.

--- .devcontainer/entrypoint.d/610_upgrade_db.sh
+++ .devcontainer/entrypoint.d/610_upgrade_db.sh
@@ -2,4 +2,5 @@
 
 # START SECTION db upgrade - if you edit these lines manually note that your change will get lost if you run the IQGeo Project Update tool
 myw_db $MYW_DB_NAME upgrade comms
+echo "running upgrade script******************************************";
 # END SECTION
