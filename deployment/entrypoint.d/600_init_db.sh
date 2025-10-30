b8e6a44ca749268539f6405a78da4f33295188f1
Andrew Wiegers <andrew@Mac.lan>
2025-10-30 16:08:38 -0600

    PLAT-12806: bash: updating 600 and 610 entry point scripts to lock if other services are initializing or upgrading database.

--- deployment/entrypoint.d/600_init_db.sh
+++ deployment/entrypoint.d/600_init_db.sh
@@ -2,6 +2,31 @@
 
 #assumes database was created (300-ensure-database script)
 
+log() {
+  echo "[$(date '+%a %b %d %H:%M:%S.%6N %Y')] $1"
+}
+
+initialize_database() {
 # START SECTION db init - if you edit these lines manually note that your change will get lost if you run the IQGeo Project Update tool
 if ! myw_db $MYW_DB_NAME list versions --layout keys | grep myw_comms_schema | grep version=; then myw_db $MYW_DB_NAME install comms; fi
 # END SECTION
+}
+
+log "Checking for shared-directory..."
+
+if [ -n "${SHARED_DIRECTORY}" ]; then
+    log "Found shared-directory attempting to lock file..."
+    LOCKFILE="${SHARED_DIRECTORY}/600_init_db.lock"
+
+    (
+    if ! flock -n 200; then
+        flock 200
+    fi
+    
+    initialize_database
+
+    ) 200>"${LOCKFILE}"
+else
+    log "shared-directory not found, upgrading database."
+    initialize_database
+fi
--- deployment/entrypoint.d/610_upgrade_db.sh
+++ deployment/entrypoint.d/610_upgrade_db.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
 
+log() {
+  echo "[$(date '+%a %b %d %H:%M:%S.%6N %Y')] $1"
+}
+
+upgrade_database() {
 if [ "${MYW_DB_UPGRADE}" == "YES" ]; then
 # START SECTION db upgrade - if you edit these lines manually note that your change will get lost if you run the IQGeo Project Update tool
 if myw_db $MYW_DB_NAME list versions --layout keys | grep myw_comms_schema | grep version=; then myw_db $MYW_DB_NAME upgrade comms; fi
 # END SECTION
+fi
+}
+
+log "Checking for shared-directory..."
+
+if [ -n "${SHARED_DIRECTORY}" ]; then
+    log "Found shared-directory attempting to lock file..."
+    LOCKFILE="${SHARED_DIRECTORY}/610_upgrade_db.lock"
+    (
+    if ! flock -n 200; then
+        flock 200
+    fi
+    
+    upgrade_database
+
+    ) 200>"${LOCKFILE}"
+else
+    log "shared-directory not found, upgrading database."
+    upgrade_database
 fi
